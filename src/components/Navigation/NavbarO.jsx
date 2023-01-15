d7a50f7f8f54b70fa9b47158b2e036f552f38408
Wojciech Pieczonka <51296602+wp9975@users.noreply.github.com>
2023-01-15 14:03:59 +0100

    navbar_active

--- src/components/Navigation/NavbarO.jsx
+++ src/components/Navigation/NavbarO.jsx
@@ -1,61 +1,64 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
 const Navbar = () => {
   const [state, setState] = useState(true);
+  const stylesLink = 'px-16 py-4 text-2xl md:text-lg md:px-6 md:py-2 bg-darkgray rounded-xl';
+  const stylesLinkActive = 'px-16 py-4 text-2xl md:text-lg md:px-6 md:py-2 bg-white text-blackCoffee underline underline-offset-2 rounded-xl';
 
   return (
     <header className="sticky z-50 top-0 left-0 bg-gray">
       <div className=" mx-auto max-w-screen-xl">
-        <div className="flex items-center justify-between py-4 md:px-10 px-5 ">
-          <div className="text-white cursor-pointer font-bold">
+        <div className="flex items-center justify-between py-4 md:px-10  ">
+          <div className="text-white cursor-pointer font-bold mx-4">
             <h1 className="text-xl">Logo</h1>
           </div>
 
           <nav
-            className={`absolute bg-gray py-12 px-5 md:py-0 flex flex-col md:flex-row md:static md:z-auto z-[-1] w-full md:w-auto items-center gap-7 text-lg ${
+            className={`absolute bg-gray py-12 text-white px-5 md:py-0 flex flex-col md:flex-row md:static md:z-auto z-[-1] w-full md:w-auto items-center gap-7 text-lg ${
               state ? "top-20" : "top-[-500px]"
             } transition-all duration-500 ease-in `}
           >
-            <Link
+            <NavLink
               to="/"
-              className="text-white transition hover:text-gray-500/75"
+              className={({isActive}) => isActive ? stylesLinkActive : stylesLink}
             >
               Strona główna
-            </Link>
+            </NavLink>
 
-            <Link
+            <NavLink
               to="/offer"
-              className="text-white transition hover:text-gray-500/75"
+              className={({isActive}) => isActive ? stylesLinkActive : stylesLink}
             >
               Oferta
-            </Link>
-            <Link
+            </NavLink>
+            <NavLink
               to="/about"
-              className="text-white transition hover:text-gray-500/75"
+              className={({isActive}) => isActive ? stylesLinkActive : stylesLink}
             >
               O nas
-            </Link>
+            </NavLink>
 
-            <Link
+            <NavLink
               to="/contact"
-              className="text-white transition hover:text-gray-500/75"
+              className={({isActive}) => isActive ? stylesLinkActive : stylesLink}
             >
               Kontakt
-            </Link>
+            </NavLink>
 
-            <Link
+            <NavLink
               to="/carlist"
-              className="text-white p-4 bg-darkgray rounded-lg transition hover:text-gray-500/75"
+              
+              className={({isActive}) => isActive ? stylesLinkActive : stylesLink}
             >
               Lista samochodów
-            </Link>
+            </NavLink>
           </nav>
 
           <div className="block md:hidden">
             <button
-              className="bg-white rounded-xl p-4 transition hover:scale-125"
+              className="bg-white mx-4 rounded-xl p-4 transition hover:scale-125"
               onClick={() => setState(!state)}
             >
               {state ? <AiOutlineClose /> : <GiHamburgerMenu />}
